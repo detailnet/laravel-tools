f9102e4977951deaac183ece21d9bae6206e96f4
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2022-04-07 20:20:56 +0200

    Fix Controller namespace

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Detail\Laravel\Http\Controllers;
+namespace Detail\Laravel\Http;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
