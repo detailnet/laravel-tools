9cc7f509a9f442c89d704f87f902ee8294ada2f2
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2023-05-09 21:57:11 +0200

    Add id as always present

--- src/Models/SortableEmbeddedModel.php
+++ src/Models/SortableEmbeddedModel.php
@@ -9,4 +9,11 @@ namespace Detail\Laravel\Models;
 abstract class SortableEmbeddedModel extends Model // Should be named SortableByDragAndDropModel
 {
     use SortByDragAndDropCommon;
+
+    protected $primaryKey = 'id';
+
+    public function getIdAttribute($value = null)
+    {
+        return $this->attributes['id'] ?? null;
+    }
 }
