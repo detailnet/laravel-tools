30a6a7fcb97f294f54997569fd7f0d898f2801b2
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2023-12-22 16:35:27 +0100

    Fix caching on unauthenticated, introduce cache TTL

--- src/Api/KeyMiddleware.php
+++ src/Api/KeyMiddleware.php
@@ -5,24 +5,25 @@ namespace Detail\Laravel\Api;
 use Closure;
 use Detail\Laravel\Http\Traits\ErrorResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use function is_string;
-use function request;
 
 class KeyMiddleware
 {
     use ErrorResponse;
 
-    protected const HEADER_APP_KEY = 'AppKey';
-
-    protected const CACHE_KEY = 'api_user'; // Set null to disable cache
+    public function __construct(
+        private ?string $cacheKey = null, // Set null to disable cache
+        private int $cacheTtl = 60,
+        private string $headerAppKey = 'AppKey'
+    )
+    {
+    }
 
     public function handle(Request $request, Closure $next): Response
     {
-        $apiKey = $request->header(static::HEADER_APP_KEY);
+        $apiKey = $request->header($this->headerAppKey);
 
         if (!is_string($apiKey)) {
             return $this->errorResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
@@ -31,23 +32,25 @@ class KeyMiddleware
         $cacheMatch = false;
         $apiUser = null;
 
-        if (is_string(static::CACHE_KEY)) {
-            $apiUser = Cache::store('api_user')->get($apiKey);
+        if (is_string($this->cacheKey)) {
+            $apiUser = Cache::store($this->cacheKey)->get($apiKey);
             $cacheMatch = $apiUser !== null;
         }
 
         if (!$cacheMatch) {
-            $apiUser = UserModel::query()->where(['active' => true, 'key' => $apiKey])->sole();
+            $apiUser = UserModel::query()->where(['active' => true, 'key' => $apiKey])->first();
         }
 
-        if (!$apiUser instanceof UserModel
-            || !$apiUser->allowsResource($request->method(), $request->path())
-        ) {
+        if (!$apiUser instanceof UserModel) {
             return $this->errorResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
         }
 
-        if (is_string(static::CACHE_KEY) && !$cacheMatch) { // Do not refresh cache if previously taken from there
-            Cache::store('api_user')->set($apiKey, $apiUser);
+        if (is_string($this->cacheKey) && !$cacheMatch) { // Do not refresh cache if previously taken from there
+            Cache::store($this->cacheKey)->set($apiKey, $apiUser, $this->cacheTtl);
+        }
+
+        if (!$apiUser->allowsResource($request->method(), $request->path())) {
+            return $this->errorResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
         }
 
         return $next($request);
